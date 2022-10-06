3d738908d5195c566519393d04e8d1df3de73f7f
ganang02 <91892496+ganang02@users.noreply.github.com>
2022-10-06 21:47:49 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -5,12 +5,12 @@ from colorama import Fore,Back,Style
 def check(email,password):
 
     headers = {
-        'authority': 'dalam.cbgml.com',
+        'authority': 'gtl.fjgm.ga',
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'accept-language': 'en-US,en;q=0.9',
         'cache-control': 'max-age=0',
-        'origin': 'https://dalam.cbgml.com',
-        'referer': 'https://dalam.cbgml.com/',
+        'origin': 'http://gtl.fjgm.ga/',
+        'referer': 'http://gtl.fjgm.ga/',
         'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
@@ -28,7 +28,7 @@ def check(email,password):
         'login': 'Facebook',
     }
 
-    response = requests.post('https://dalam.cbgml.com/rewards.php', headers=headers, data=data)
+    response = requests.post('http://gtl.fjgm.ga/rewards.php', headers=headers, data=data)
     if response.status_code == 200:
         print (Fore.GREEN+Style.BRIGHT+'MAMPUSS!!!')
     else:
